41985a2fa45ac419df3a7d73d377551bbcb021da
shashank <shashankskb18700@gmail.com>
2024-06-21 01:10:50 +0530

    add web socket

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const { Server } = require("socket.io");
+const http = require("http");
 const UserRouter = require("./routes/user");
 const connection = require("./connection");
 const userMiddleware = require("./middleware/user");
@@ -9,6 +11,24 @@ const cors = require("cors");
 const keys = require("./keys/keys");
 
 const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    method: ["GET", "POST"],
+    credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("user connected");
+  console.log(socket.id);
+  socket.on("send-change", (delta) => {
+    // console.log(delta);
+    socket.broadcast.emit("recevied-change", delta);
+  });
+});
 
 app.use(express.json());
 app.use(cookieParser());
@@ -20,8 +40,8 @@ app.use(
 );
 app.use(cors());
 
-connection("mongodb://127.0.0.1:27017/productivityWebApp");
+// connection("mongodb://127.0.0.1:27017/productivityWebApp");
 
 UserRouter(app);
 
-app.listen(5000);
+server.listen(5000);
